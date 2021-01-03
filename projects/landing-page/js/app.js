bf2af74a2c48a9a11106b1f90b34ca21b1618b16
samad <samadarshad@hotmail.co.uk>
2021-01-03 16:04:57 +0000

    nav bar and scrolling

--- projects/landing-page/js/app.js
+++ projects/landing-page/js/app.js
@@ -11,32 +11,61 @@
  * 
  * JS Standard: ESlint
  * 
-*/
+ */
 
 /**
  * Define Global Variables
  * 
-*/
+ */
 
 
 /**
  * End Global Variables
  * Start Helper Functions
  * 
-*/
-
+ */
+function respondToTheClick(e) {
+    e.preventDefault();
+    console.log(e);
+    console.log('A paragraph was clicked.' + e.target.firstElementChild.getAttribute("href"));
+    const id = e.target.firstElementChild.getAttribute("href");
+    const element = document.getElementById(id);
+    const topOfElement = element.getBoundingClientRect().top + window.pageYOffset;
+    console.log(topOfElement)
+    window.scrollTo({
+        top: topOfElement,
+        left: 0,
+        behavior: 'smooth'
+    })
+}
 
 
 /**
  * End Helper Functions
  * Begin Main Functions
  * 
-*/
+ */
 
 // build the nav
+const navBar = document.getElementById("navbar__list");
+const fragment = document.createDocumentFragment();
+const sections = document.getElementsByTagName("section");
+for (const section of sections) {
+    const newNavEl = document.createElement('li');
+    newNavEl.innerText = section.getAttribute("data-nav");
+    newNavEl.classList.add("menu__link");
 
+    const anchor = document.createElement('a');
+    anchor.setAttribute('href', section.getAttribute('id'));
+    newNavEl.appendChild(anchor);
 
+    fragment.appendChild(newNavEl);
+}
+navBar.appendChild(fragment);
 // Add class 'active' to section when near top of viewport
+
+
+navBar.addEventListener('click', respondToTheClick);
 
 
 // Scroll to anchor ID using scrollTO event
@@ -46,12 +75,10 @@
  * End Main Functions
  * Begin Events
  * 
-*/
+ */
 
 // Build menu 
 
 // Scroll to section on link click
 
 // Set sections as active
-
-
