4e9a8e2a8508b3c2534818b8d772998b3381f44d
samad <samadarshad@hotmail.co.uk>
2021-01-06 04:05:38 +0000

    working sending data from client to server

--- projects/weather-journal-app/shared/share.js
+++ projects/weather-journal-app/shared/share.js
@@ -5,11 +5,12 @@
     };
 
     exports.dataScheme = class {
-         constructor(temperatureDegreesCelcius, date, user_input) {
+         constructor(temperatureDegreesCelcius, date, user_input, zip) {
               this.temperatureDegreesCelcius = temperatureDegreesCelcius;
               this.date = date;
               this.user_input = user_input;
+              this.zip = zip;
          }
-    }
+    };
   
   }(typeof exports === 'undefined' ? this.share = {} : exports));
--- projects/weather-journal-app/website/app.js
+++ projects/weather-journal-app/website/app.js
@@ -1,23 +1,20 @@
-/* Global Variables */
-
-// Create a new date instance dynamically with JS
-const d = new Date();
-const newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
-
 console.log(share.test());
-console.log(newDate);
+
 
 const generate = document.getElementById("generate");
 const feelings = document.getElementById("feelings");
+const zip = document.getElementById("zip");
 
 generate.addEventListener('click', respondToClick);
 
 async function respondToClick () {
     console.log("clicked")
     console.log(feelings.value);
-    await requests.postData('/api/add', {'data':'test2'});
+    const newDate = new Date().toDateString();
+    const newEntry = new share.dataScheme(null, newDate, feelings.value, zip.value);
+    await requests.postData('/api/add', newEntry);
     const data = await requests.getData('/api/all');
-
+    
     console.log(data);
     // window.location.reload(true); 
 }
