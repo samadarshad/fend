255c395c55eba9d35c3599394e5f967eced4ddf1
samad <samadarshad@hotmail.co.uk>
2021-01-09 18:11:57 +0000

    routes working

--- projects/evaluate-news-nlp/src/server/index.js
+++ projects/evaluate-news-nlp/src/server/index.js
@@ -4,6 +4,7 @@ const mockAPIResponse = require('./mockAPI.js')
 var bodyParser = require('body-parser')
 var cors = require('cors')
 const fetch = require('node-fetch');
+const routes = require('./routes');
 
 const app = express()
 const dotenv = require('dotenv');
@@ -15,22 +16,22 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({
     extended: true
   }))
-
+app.use('/api', routes);
 console.log(__dirname)
 
-app.get('/', function (req, res) {
-    res.sendFile('dist/index.html')
-    // res.sendFile(path.resolve('src/client/views/index.html'))
-})
+// app.get('/', function (req, res) {
+//     res.sendFile('dist/index.html')
+//     // res.sendFile(path.resolve('src/client/views/index.html'))
+// })
 
 // designates what port the app will listen to for incoming requests
 app.listen(8081, function () {
     console.log('Example app listening on port 8081!')
 })
 
-app.get('/test', function (req, res) {
-    res.send(mockAPIResponse)
-})
+// app.get('/test', function (req, res) {
+//     res.send(mockAPIResponse)
+// })
 
   console.log(`Your API key is ${process.env.TEXT_API_KEY}`);
 
---
+++ projects/evaluate-news-nlp/src/server/routes.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const router = express.Router();
+const mockAPIResponse = require('./mockAPI.js')
+
+function sendErrorToClient(error, res) {
+    res.status(error.message).send(error)  
+}
+
+router.post('/add', async function(req, res) {
+    res.send({success: "ok"})
+})
+
+router.get('/', function (req, res) {
+    res.sendFile('dist/index.html')
+    // res.sendFile(path.resolve('src/client/views/index.html'))
+})
+
+router.get('/test', function (req, res) {
+    res.send(mockAPIResponse)
+})
+
+module.exports = router;
