d9cbcdeec18e79828175484309b9b909fc0bed2c
samad <samadarshad@hotmail.co.uk>
2021-01-09 14:18:45 +0000

    working api call

--- projects/evaluate-news-nlp/src/client/index.js
+++ projects/evaluate-news-nlp/src/client/index.js
@@ -16,3 +16,5 @@ export {
     checkForName,
     handleSubmit
 }
+
+console.log("hello")
--- projects/evaluate-news-nlp/src/server/index.js
+++ projects/evaluate-news-nlp/src/server/index.js
@@ -3,8 +3,8 @@ const express = require('express')
 const mockAPIResponse = require('./mockAPI.js')
 var bodyParser = require('body-parser')
 var cors = require('cors')
+const fetch = require('node-fetch');
 
-var aylien = require("aylien_textapi");
 const app = express()
 const dotenv = require('dotenv');
 dotenv.config();
@@ -32,9 +32,45 @@ app.get('/test', function (req, res) {
     res.send(mockAPIResponse)
 })
 
-var textapi = new aylien({
-    application_id: process.env.AYLIEN_API_ID,
-    application_key: process.env.AYLIEN_API_KEY
+  console.log(`Your API key is ${process.env.TEXT_API_KEY}`);
+
+//   fetch(`https://api.meaningcloud.com/sentiment-2.1?key=${process.env.TEXT_API_KEY}&of=json&txt=Main%20dishes%20were%20quite%20good%2C%20but%20desserts%20were%20too%20sweet%20for%20me.&lang=en`)
+//   .then(res => res.json())
+//   .then(function(res) {
+//     console.log(res)
+//     //   document.getElementById('results').innerHTML = res.message
+//   })
+
+  console.log("hello")
+
+
+  var https = require('follow-redirects').https;
+  var fs = require('fs');
+
+  var options = {
+    'method': 'POST',
+    'hostname': 'api.meaningcloud.com',
+    'path': `/sentiment-2.1?key=${process.env.TEXT_API_KEY}&lang=en&txt=Main%20dishes%20were%20quite%20good%2C%20but%20desserts%20were%20too%20sweet%20for%20me.`,
+    'headers': {
+    },
+    'maxRedirects': 20
+  };
+
+  var req = https.request(options, function (res) {
+    var chunks = [];
+
+    res.on("data", function (chunk) {
+      chunks.push(chunk);
+    });
+
+    res.on("end", function (chunk) {
+      var body = Buffer.concat(chunks);
+      console.log(body.toString());
+    });
+
+    res.on("error", function (error) {
+      console.error(error);
+    });
   });
 
-  console.log(`Your API key is ${process.env.AYLIEN_API_KEY}`);
+  req.end();
