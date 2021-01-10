9c974b4e0ccec01b22594ea57599c32d95e929f7
samad <samadarshad@hotmail.co.uk>
2021-01-10 21:10:58 +0000

    cleaning

--- projects/evaluate-news-nlp/src/client/js/formHandler.js
+++ projects/evaluate-news-nlp/src/client/js/formHandler.js
@@ -1,7 +1,8 @@
 export async function respondToSubmit (event) {
     try {
         event.preventDefault()
-        const data = await Client.sendForm()
+        let formText = document.getElementById('name').value
+        const data = await Client.sendForm(formText)
         await Client.updateUI(data);
     } catch (error) {
         console.log("respondToSubmit error", error);
@@ -10,12 +11,12 @@ export async function respondToSubmit (event) {
 
 console.log(`SERVER_BASE_ENDPOINT_URL ${SERVER_BASE_ENDPOINT_URL}`)
 
-export async function sendForm() {
-    let formText = document.getElementById('name').value
-    const requests = Client.clientSideRequests()
-    const messageMaker = new Client.messageScheme();
-    const jsonMessage = messageMaker.getJson(formText)    
-    const res = await requests.postData(`${SERVER_BASE_ENDPOINT_URL}/sentiment`, jsonMessage);
+export async function sendForm(text) {
+    
+    // const requests = Client.clientSideRequests()
+    // const messageMaker = new Client.messageScheme();
+    const jsonMessage = new Client.messageScheme().getJson(text)    
+    const res = await Client.clientSideRequests().postData(`${SERVER_BASE_ENDPOINT_URL}/sentiment`, jsonMessage);
     return res
 }
 
