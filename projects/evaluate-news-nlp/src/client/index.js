cac2bc54c56591989334d69436f7e633b698e9a5
samad <samadarshad@hotmail.co.uk>
2021-01-12 16:52:56 +0000

    cleaning

--- projects/evaluate-news-nlp/src/client/index.js
+++ projects/evaluate-news-nlp/src/client/index.js
@@ -1,13 +1,9 @@
-// import "regenerator-runtime/runtime";
-
 import './styles/resets.scss'
 import './styles/base.scss'
 import './styles/form.scss'
 import './styles/footer.scss'
 import './styles/header.scss'
 
-//TODO the below shouldnt be exported into Client Library - there should be a ClientLib.js and an index.js as output
-// perhaps the below could go onto index.html as a script?
 const fetch = window.fetch.bind(window);
 Client.setFetch(fetch)
 Client.setServerUrl("/api")
@@ -15,4 +11,3 @@ let user_input = document.getElementsByClassName('user-input');
 for (const element of user_input) {
     element.addEventListener("submit", e => Client.respondToSubmit(e) );
 }
-
