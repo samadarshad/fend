b559c151e9a6139e157d943a1d3f1347d0546e0f
samad <samadarshad@hotmail.co.uk>
2021-01-12 16:01:43 +0000

    tests work with lib separated

--- projects/evaluate-news-nlp/src/client/js/formHandler.js
+++ projects/evaluate-news-nlp/src/client/js/formHandler.js
@@ -1,5 +1,3 @@
-const SERVER_BASE_ENDPOINT_URL = process.env.SERVER_BASE_ENDPOINT_URL
-
 export async function respondToSubmit (event) {
     try {
         event.preventDefault()
@@ -10,8 +8,6 @@ export async function respondToSubmit (event) {
         console.log("respondToSubmit error", error);
     }
 }
-
-console.log(`SERVER_BASE_ENDPOINT_URL ${SERVER_BASE_ENDPOINT_URL}`)
 
 export async function sendForm(text) {
     const jsonMessage = new Client.messageScheme().getJson(text)
--- projects/evaluate-news-nlp/webpack.prod.ClientLib.js
+++ projects/evaluate-news-nlp/webpack.prod.ClientLib.js
@@ -1,12 +1,7 @@
 const path = require('path')
-const webpack = require('webpack')
-const HtmlWebPackPlugin = require("html-webpack-plugin")
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 const TerserPlugin = require('terser-webpack-plugin');
-const WorkboxPlugin = require('workbox-webpack-plugin');
-const dotenv = require('dotenv')
-const client_env = dotenv.config({path:__dirname+'/src/client/.env'}).parsed;
 
 module.exports = {
     entry: {
@@ -42,16 +37,5 @@ module.exports = {
                 use: [ MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader' ]
             }
         ]
-    },
-    plugins: [
-        new HtmlWebPackPlugin({
-            template: "./src/client/views/index.html",
-            filename: "./index.html",
-        }),
-        new MiniCssExtractPlugin({filename: '[name].css'}),
-        new WorkboxPlugin.GenerateSW(),
-        new webpack.DefinePlugin({
-            "process.env": JSON.stringify(client_env)
-        })
-    ]
+    }
 }
