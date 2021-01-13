78a0edbd9b38ebf5adf3157662f254e307577ca6
samad <samadarshad@hotmail.co.uk>
2021-01-13 09:22:27 +0000

    cleaning, tests working

--- projects/evaluate-news-nlp/__test__/formHandler.test.js
+++ projects/evaluate-news-nlp/__test__/formHandler.test.js
@@ -1,22 +1,17 @@
-require('jest-fetch-mock').enableMocks()
-// import "regenerator-runtime/runtime";
-// import { sendForm } from "../src/client/js/formHandler"
-// import { Client, sendForm } from "../dist/ClientLib.js"
+const fetchMock = require('jest-fetch-mock')
+fetchMock.enableMocks()
 global.Client = require('../dist/ClientLib')
-// const fetch = require('node-fetch')
-
 
 describe('formHandler', () => {
-
-    // jest.mock('node-fetch')
-    
-    it('sendForm is defined', async done => {
-        fetch.mockResponse(JSON.stringify({ data: '12345' }))
-        // fetchMock.mockResponse({message: "mzck response"})
-        Client.setFetch(global.fetch)
-        const res = await Client.sendForm("Hello") // this hits fetch at /api/sentiment, and that needs to be mocked/proxied
-        console.log(res)
-        // expect(res).toBeDefined()
+    it('when sendForm then expect /api/sentiment to be called', async done => {
+        fetchMock.mockResponse(JSON.stringify({ data: '12345' }))
+        Client.setFetch(fetchMock)
+        const res = await Client.sendForm("Hello")
+        
+        expect(fetchMock.mock.calls.length).toEqual(1)
+        expect(fetchMock.mock.calls[0][0]).toEqual('/api/sentiment')
+        expect(fetchMock.mock.calls[0][1].body).toEqual(JSON.stringify({ message: 'Hello' }))
+        expect(res.data).toEqual('12345')
         done()
     })
 
