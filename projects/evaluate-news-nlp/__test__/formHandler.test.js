d8134f6c679514f9492f186c9be8c8c94ce9ca4d
samad <samadarshad@hotmail.co.uk>
2021-01-13 09:16:05 +0000

    mocking fetch works

--- projects/evaluate-news-nlp/__test__/formHandler.test.js
+++ projects/evaluate-news-nlp/__test__/formHandler.test.js
@@ -1,13 +1,19 @@
+require('jest-fetch-mock').enableMocks()
 // import "regenerator-runtime/runtime";
 // import { sendForm } from "../src/client/js/formHandler"
 // import { Client, sendForm } from "../dist/ClientLib.js"
 global.Client = require('../dist/ClientLib')
-const fetch = require('node-fetch')
+// const fetch = require('node-fetch')
+
 
 describe('formHandler', () => {
+
+    // jest.mock('node-fetch')
     
     it('sendForm is defined', async done => {
-        Client.setFetch(fetch)
+        fetch.mockResponse(JSON.stringify({ data: '12345' }))
+        // fetchMock.mockResponse({message: "mzck response"})
+        Client.setFetch(global.fetch)
         const res = await Client.sendForm("Hello") // this hits fetch at /api/sentiment, and that needs to be mocked/proxied
         console.log(res)
         // expect(res).toBeDefined()
